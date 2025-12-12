d12e6b129183135f19e4a8b92e6bea7f49160422
Axel Polleux <apolleux@2A1.42angouleme.fr>
2025-12-12 19:51:21 +0100

    First libft programs

---
+++ libft/ft_atoi.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apolleux <apolleux@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/18 13:28:51 by apolleux          #+#    #+#             */
+/*   Updated: 2025/10/18 15:21:44 by apolleux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	sign;
+	int	res;
+
+	sign = 1;
+	res = 0;
+	while (*nptr == ' ' || (9 <= *nptr && *nptr <= 13))
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	while (ft_isdigit(*nptr))
+	{
+		res *= 10;
+		res += *nptr - '0';
+		nptr++;
+	}
+	return (res * sign);
+}
