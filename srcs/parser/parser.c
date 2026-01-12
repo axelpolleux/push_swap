537cff20394be86473a48bc559f76901841575e5
apolleux <polleuxaxel@gmail.com>
2026-01-12 11:49:49 +0100

    Test printf in CLIOn

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: apolleux <apolleux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 11:49:43 by apolleux          #+#    #+#             */
-/*   Updated: 2026/01/12 10:34:54 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/01/12 11:49:07 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,6 +87,11 @@ int	parser(int argc, char	**argv, t_node *stack_a)
 	if (!check_arg(args))
 		return (0);
 	i = 0;
+	while (args[i])
+	{
+		ft_printf("%s\n", args[i]);
+		i++;
+	}
 	stack_a = link_manage(args);
 	return (1);
 }
