8ad2b654364f861135fd1f8b89a1c0be92ced6a9
apolleux <polleuxaxel@gmail.com>
2026-01-27 09:51:00 +0100

    Clear list normed

--- srcs/parser/clear_list.c
+++ srcs/parser/clear_list.c
@@ -6,7 +6,7 @@
 /*   By: apolleux <apolleux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 15:34:59 by apolleux          #+#    #+#             */
-/*   Updated: 2026/01/23 15:38:20 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/01/27 09:44:47 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	clear_list(t_node **stack)
 {
 	t_node	*tmp;
 
-	while (*stack){
+	while (*stack)
+	{
 		if ((*stack)->next)
 			tmp = (*stack)->next;
-
 	}
 }
